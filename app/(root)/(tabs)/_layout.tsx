f85abd60ecec908bc94b8d4de3106481949f4929
pratap <pratapg739@gmail.com>
2025-04-15 01:52:54 +0530

    Drawer additional implementation changes and updated services page images.

--- app/(root)/(tabs)/_layout.tsx
+++ app/(root)/(tabs)/_layout.tsx
@@ -4,58 +4,101 @@ import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { Ionicons } from "@expo/vector-icons";
 import { Text } from "react-native";
 import { useUser } from "@clerk/clerk-expo";
+import { usePathname } from "expo-router";
+import CustomDrawerContent from "@/components/CustomDrawerContent";
 // import { SafeAreaView } from "react-native-safe-area-context";
 
 const DrayerLayout = () => {
 	const { user } = useUser();
+	const path = usePathname();
 
 	return (
-		<GestureHandlerRootView className="">
-			<Drawer>
+		<GestureHandlerRootView>
+			<Drawer drawerContent={CustomDrawerContent}>
 				<Drawer.Screen
 					name="home"
 					options={{
-						drawerLabel: "Home",
+						drawerLabel: () => (
+							<Text
+								className={`font-JakartaMedium !text-[${path === "/home" ? "#fff" : "#000"}]`}>
+								Home
+							</Text>
+						),
 						headerTitle: () => (
-							<Text className="text-2xl font-JakartaExtraBold">
+							<Text className={`text-2xl font-JakartaExtraBold`}>
 								{user?.emailAddresses?.[0]
 									? user?.emailAddresses?.[0]?.emailAddress?.split("@")?.[0]
 									: "Pratap"}
 							</Text>
 						),
-						drawerIcon: ({ size, color }) => (
-							<Ionicons name="home-outline" size={size} color={color} />
+						drawerIcon: ({ size }) => (
+							<Ionicons
+								name="home-outline"
+								size={size}
+								color={path === "/home" ? "#fff" : "#000"}
+							/>
 						),
+						drawerActiveBackgroundColor: "#000",
 					}}
 				/>
 				<Drawer.Screen
 					name="services"
 					options={{
-						drawerLabel: "Services",
-						headerTitle: "Beauty Services",
-						drawerIcon: ({ size, color }) => (
-							<Ionicons name="rocket" size={size} color={color} />
+						drawerLabel: () => (
+							<Text
+								className={`font-JakartaMedium !text-[${path === "/services" ? "#fff" : "#000"}]`}>
+								Services
+							</Text>
 						),
+						headerTitle: "Beauty Services",
+						drawerIcon: ({ size }) => (
+							<Ionicons
+								name="rocket"
+								size={size}
+								color={path === "/services" ? "#fff" : "#000"}
+							/>
+						),
+						drawerActiveBackgroundColor: "#000",
 					}}
 				/>
 				<Drawer.Screen
 					name="orders"
 					options={{
-						drawerLabel: "Your Orders",
-						headerTitle: "Orders",
-						drawerIcon: ({ size, color }) => (
-							<Ionicons name="bag-outline" size={size} color={color} />
+						drawerLabel: () => (
+							<Text
+								className={`font-JakartaMedium !text-[${path === "/orders" ? "#fff" : "#000"}]`}>
+								Your Orders
+							</Text>
 						),
+						headerTitle: "Orders",
+						drawerIcon: ({ size }) => (
+							<Ionicons
+								name="bag-outline"
+								size={size}
+								color={path === "/orders" ? "#fff" : "#000"}
+							/>
+						),
+						drawerActiveBackgroundColor: "#000",
 					}}
 				/>
 				<Drawer.Screen
 					name="profile"
 					options={{
-						drawerLabel: "Profile",
-						headerTitle: "Profile",
-						drawerIcon: ({ size, color }) => (
-							<Ionicons name="person-outline" size={size} color={color} />
+						drawerLabel: () => (
+							<Text
+								className={`font-JakartaMedium !text-[${path === "/profile" ? "#fff" : "#000"}]`}>
+								Profile
+							</Text>
 						),
+						headerTitle: "Profile",
+						drawerIcon: ({ size }) => (
+							<Ionicons
+								name="person-outline"
+								size={size}
+								color={path === "/profile" ? "#fff" : "#000"}
+							/>
+						),
+						drawerActiveBackgroundColor: "#000",
 					}}
 				/>
 			</Drawer>
