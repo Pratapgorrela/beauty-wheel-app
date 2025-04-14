4cfacd5a7909b27391fc6d0ae86a057f4f2922de
pratap <pratapg739@gmail.com>
2025-04-14 08:11:10 +0530

    css changes for map

--- app/(root)/(tabs)/_layout.tsx
+++ app/(root)/(tabs)/_layout.tsx
@@ -12,54 +12,52 @@ const DrayerLayout = () => {
 	return (
 		<GestureHandlerRootView className="">
 			<Drawer>
-				<SafeAreaView>
-					<Drawer.Screen
-						name="home"
-						options={{
-							drawerLabel: "Home",
-							headerTitle: () => (
-								<Text className="text-2xl font-JakartaExtraBold">
-									{user?.emailAddresses?.[0]
-										? user?.emailAddresses?.[0]?.emailAddress?.split("@")?.[0]
-										: "Pratap"}
-								</Text>
-							),
-							drawerIcon: ({ size, color }) => (
-								<Ionicons name="home-outline" size={size} color={color} />
-							),
-						}}
-					/>
-					<Drawer.Screen
-						name="services"
-						options={{
-							drawerLabel: "Services",
-							headerTitle: "Beauty Services",
-							drawerIcon: ({ size, color }) => (
-								<Ionicons name="rocket" size={size} color={color} />
-							),
-						}}
-					/>
-					<Drawer.Screen
-						name="orders"
-						options={{
-							drawerLabel: "Your Orders",
-							headerTitle: "Orders",
-							drawerIcon: ({ size, color }) => (
-								<Ionicons name="bag-outline" size={size} color={color} />
-							),
-						}}
-					/>
-					<Drawer.Screen
-						name="profile"
-						options={{
-							drawerLabel: "Profile",
-							headerTitle: "Profile",
-							drawerIcon: ({ size, color }) => (
-								<Ionicons name="person-outline" size={size} color={color} />
-							),
-						}}
-					/>
-				</SafeAreaView>
+				<Drawer.Screen
+					name="home"
+					options={{
+						drawerLabel: "Home",
+						headerTitle: () => (
+							<Text className="text-2xl font-JakartaExtraBold">
+								{user?.emailAddresses?.[0]
+									? user?.emailAddresses?.[0]?.emailAddress?.split("@")?.[0]
+									: "Pratap"}
+							</Text>
+						),
+						drawerIcon: ({ size, color }) => (
+							<Ionicons name="home-outline" size={size} color={color} />
+						),
+					}}
+				/>
+				<Drawer.Screen
+					name="services"
+					options={{
+						drawerLabel: "Services",
+						headerTitle: "Beauty Services",
+						drawerIcon: ({ size, color }) => (
+							<Ionicons name="rocket" size={size} color={color} />
+						),
+					}}
+				/>
+				<Drawer.Screen
+					name="orders"
+					options={{
+						drawerLabel: "Your Orders",
+						headerTitle: "Orders",
+						drawerIcon: ({ size, color }) => (
+							<Ionicons name="bag-outline" size={size} color={color} />
+						),
+					}}
+				/>
+				<Drawer.Screen
+					name="profile"
+					options={{
+						drawerLabel: "Profile",
+						headerTitle: "Profile",
+						drawerIcon: ({ size, color }) => (
+							<Ionicons name="person-outline" size={size} color={color} />
+						),
+					}}
+				/>
 			</Drawer>
 		</GestureHandlerRootView>
 	);
